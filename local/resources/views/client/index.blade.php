78cb1156cfc7b8082ec328795854d72432c31ffc
boyvn2911 <boyvn2911@gmail.com>
2018-10-12 10:40:08 +0700

    Merge branch 'master' of https://github.com/ldemon1005/acland
    # Conflicts:
    #	local/resources/views/client/index.blade.php

--- local/resources/views/client/index.blade.php
+++ local/resources/views/client/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>AC LAND</title>
-
+    <link rel="icon" href="{{asset('local/storage/app/client/images/logo.png')}}" type="image/x-icon" />
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="stylesheet" href="{{asset('local/storage/app/client/css/reset.css')}}">
